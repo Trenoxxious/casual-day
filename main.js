457a2c64c5e42b25e7a186bc46432b46da7375ae
Noxxious <trenoxxious@gmail.com>
2025-08-23 16:44:14 +0000

    Added Support for Weekend Days to always be considered casual, added icon, added title change support

--- main.js
+++ main.js
@@ -7,14 +7,17 @@ const notSure = document.querySelector('.not-sure');
 const date = new Date();
 const dayOfTheMonth = date.getDate();
 const fullMonthName = date.toLocaleString('default', { month: 'long' });
+const fullDayName = date.toLocaleString('default', { weekday: 'long' });
 const monthIndex = casualCalendar.findIndex(data => data.month === fullMonthName);
+
+const weekendsAreCasual = true;
 
 // We check for casual day here
 function getCasualDay(month, day) {
     if (month === -1) return notSure.style.display = 'flex';
 
     const monthToCheck = casualCalendar[month].days;
-    const isItCasual = monthToCheck.findIndex(casualDay => casualDay === day) !== -1;
+    const isItCasual = monthToCheck.findIndex(casualDay => casualDay === day) !== -1 || ((fullDayName.toLowerCase() === "saturday" || fullDayName.toLowerCase() === "sunday") && weekendsAreCasual);
 
     const lockedBonusDays = bonusDays[month].locked;
     const lockedDay = lockedBonusDays.findIndex(locked => locked === day) !== -1;
@@ -22,6 +25,7 @@ function getCasualDay(month, day) {
     const unlockedDay = unlockedBonusDays.findIndex(unlocked => unlocked === day) !== -1;
 
     if (isItCasual) {
+        document.title = `Is It Casual Day? Yes!`;
         yesItIs.style.display = 'flex';
         document.body.style = `background: linear-gradient(to bottom left, rgba(33, 68, 47, 1), rgb(26, 26, 26));`;
         if (unlockedDay) {
@@ -36,8 +40,9 @@ function getCasualDay(month, day) {
             `;
         }
     } else {
+        document.title = `Is It Casual Day? No.`;
         noItIsnt.style.display = 'flex';
-        document.body.style = `background: linear-gradient(to bottom left, rgba(68, 66, 33, 1), rgb(26, 26, 26));`;
+        document.body.style = `background: linear-gradient(to bottom left, rgba(68, 33, 33, 1), rgb(26, 26, 26));`;
         if (lockedDay) {
             noItIsnt.innerHTML = `
                 <span style="color: rgb(255, 254, 243);">No, it is <b>not</b> a bonus casual day.</span>
