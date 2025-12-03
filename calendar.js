257ca06c224b4de1f864555af41ba7b3f400b4a6
Noxxious <trenoxxious@gmail.com>
2025-12-03 18:14:36 +0000

    Update to Casual Days

--- calendar.js
+++ calendar.js
@@ -1,34 +1,20 @@
 // Keep this to 3 rolling months maximum to avoid overlapping years
 const casualCalendar = [
     {
-        month: "August",
-        days: [4, 6, 7, 11, 13, 15, 19, 21, 22, 25, 26, 27, 28, 29],
-    },
-    {
-        month: "September",
-        days: [1, 4, 8, 9, 10, 11, 17, 18, 19, 20, 22, 25],
-    },
-    {
         month: "October",
         days: [1, 2, 6, 7, 8, 9, 10, 13, 15, 21, 24, 30, 31],
     },
     {
         month: "November",
         days: [4, 6, 11, 14, 17, 18, 19, 20, 21, 26, 27],
+    },
+    {
+        month: "December",
+        days: [2, 5, 8, 12, 15, 18, 23, 24, 25, 31],
     }
 ];
 
 const bonusDays = [
-    {
-        month: "August",
-        unlocked: [7, 13, 22],
-        locked: [18],
-    },
-    {
-        month: "September",
-        unlocked: [12, 15, 24, 30],
-        locked: [],
-    },
     {
         month: "October",
         unlocked: [14, 27, 29],
@@ -38,5 +24,10 @@ const bonusDays = [
         month: "November",
         unlocked: [10, 13, 25],
         locked: [],
-    }
+    },
+    {
+        month: "December",
+        unlocked: [],
+        locked: [11, 17, 26, 30],
+    },
 ];
