6d20059e2c565289f31b42ca32dcc3695fbd9a38
Noxxious <trenoxxious@gmail.com>
2025-12-31 17:49:43 +0000

    January Calendar (no bonus casuals)

--- calendar.js
+++ calendar.js
@@ -1,25 +1,20 @@
 // Keep this to 3 rolling months maximum to avoid overlapping years
 const casualCalendar = [
     {
-        month: "October",
-        days: [1, 2, 6, 7, 8, 9, 10, 13, 15, 21, 24, 30, 31],
-    },
-    {
         month: "November",
         days: [4, 6, 11, 14, 17, 18, 19, 20, 21, 26, 27],
     },
     {
         month: "December",
         days: [2, 5, 8, 12, 15, 18, 23, 24, 25, 31],
-    }
+    },
+    {
+        month: "January",
+        days: [1, 7, 8, 13, 15, 19, 21, 27, 30],
+    },
 ];
 
 const bonusDays = [
-    {
-        month: "October",
-        unlocked: [14, 27, 29],
-        locked: [23],
-    },
     {
         month: "November",
         unlocked: [10, 13, 25],
@@ -28,6 +23,11 @@ const bonusDays = [
     {
         month: "December",
         unlocked: [11, 17, 26, 29, 30],
-        locked: [26],
+        locked: [],
+    },
+    {
+        month: "January",
+        unlocked: [],
+        locked: [],
     },
 ];
